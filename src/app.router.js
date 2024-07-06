d460b765adcf11b9e1cf9af80dbedb0fb19dc393
Tasneem <tasneemgazal5@gmail.com>
2024-07-06 22:41:56 +0300

    fix issue in global error handler

--- src/app.router.js
+++ src/app.router.js
@@ -3,22 +3,25 @@ import adminRouter from "./modules/admin/admin.router.js";
 import authRouter from "./modules/auth/auth.router.js";
 import userRouter from "./modules/user/user.router.js";
 
-const initApp = (app,express)=>{
-    connectDB();
-    app.use(express.json());
-    app.get('/', (req, res)=>{
-        return res.status(200).json({message:"success"});
-    })
-    app.use('/auth',authRouter);
-    app.use('/admin',adminRouter);
-    app.use('/user',userRouter);
-    app.use('*', (req, res)=>{
-        return res.status(404).json({message:"page not found"});
-    })
-    app.use((err,req,res,next)=>{
-        return res.status(err.statusCode).json({message:err.message});
-    })
+const initApp = (app, express) => {
+  connectDB();
+  app.use(express.json());
 
-}
+  app.get('/', (req, res) => {
+    return res.status(200).json({ message: "success" });
+  });
+
+  app.use('/auth', authRouter);
+  app.use('/admin', adminRouter);
+  app.use('/user', userRouter);
+
+  app.use('*', (req, res) => {
+    return res.status(404).json({ message: "page not found" });
+  });
+
+  app.use((err, req, res, next) => {
+    return res.status(err.statusCode).json({ message: err.message });
+  });
+};
+
 export default initApp;
- 
