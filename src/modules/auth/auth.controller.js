3db40d091f91db5ddf98ad95f1b3f1b22aea28d0
Tasneem <tasneemgazal5@gmail.com>
2024-07-06 22:45:09 +0300

    creat signIn function and add its router

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,7 +1,51 @@
+import studentModel from "../../../DB/models/student.model.js";
 import userModel from "../../../DB/models/user.model.js";
-
-export const signUp = async(req,res,next)=>{
-const{userName,email,password} = req.body;
-const user = await userModel.create({userName:userName,email:email,password:password});
-return res.json(user)
-}
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { AppError } from "../../utils/AppError.js";
+export const signIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        let user = await userModel.findOne({
+          email,
+        });
+        if (!user) {
+          user = await studentModel.findOne({
+            email: email,
+          });
+        }
+        if (!user) {
+          return next(new AppError("invalid account",404 ));
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return next(
+            new AppError("invalid account error in password", 400)
+          );
+        }
+        let tokenPayload = {
+          _id: user._id,
+          email: user.email,
+          name: user.name,
+          depId:user.depId,
+          img:user.img,
+          phoneNumber:user.phoneNumber,
+          role:user.role
+        };
+        if (user instanceof studentModel) {
+          tokenPayload.academicYear = user.academicYear;
+        } else {
+          tokenPayload.officeHours = user.officeHours;
+        }
+        const token = jwt.sign(
+          tokenPayload,
+          process.env.LOGINTOKEN,
+          {
+            expiresIn: 60 * 60 * 24 * 7 ,
+          }
+        );
+        return res.status(200).json({ message: "valid account", token, role: user.role });
+      } catch (err) {
+        next(new Error(err.message, { cause: 500 }));
+      }
+};
--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,5 +1,5 @@
 import { Router} from "express";
 import * as Auth from "./auth.controller.js";
 const authRouter = Router();
-authRouter.post('/',Auth.signUp);
+authRouter.post('/login',Auth.signIn)
 export default authRouter;
