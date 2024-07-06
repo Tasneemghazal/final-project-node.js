acad79291f2c655e136fd14949cf61e2f01406bd
Tasneem <tasneemgazal5@gmail.com>
2024-07-06 22:44:44 +0300

    add admin crud router for user

--- src/modules/admin/admin.router.js
+++ src/modules/admin/admin.router.js
@@ -1,7 +1,13 @@
 import {Router} from 'express';
 import * as admin from './admin.controller.js';
+import uploadFile, { fileTypes } from '../../utils/multer.js';
+import { checkEmail } from '../../middleware/checkEmail.middleware.js';
+import { checkDep } from '../../middleware/checkDep.middleware.js';
+import { auth } from '../../middleware/auth.middleware.js';
+import { role } from '../../utils/role.js';
 const adminRouter =Router();
 
 adminRouter.post('/addDep',admin.addDepartment);
-
+adminRouter.post('/addUser',auth([role.admin]),uploadFile(fileTypes.image).single("img"),checkEmail,checkDep,admin.addUser);
+adminRouter.get('/getUsers',auth([role.admin]),admin.getUsers);
 export default adminRouter;
