17f8f9ef487a8aa494770ac6928426abf148737b
Tasneem <tasneemgazal5@gmail.com>
2024-07-10 11:35:49 +0300

    handle error in all endpoints using asyncHandler

--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -1,19 +1,25 @@
 import departmentModel from "../../../DB/models/department.model.js";
 import userModel from "../../../DB/models/user.model.js";
-import { AppError } from "../../utils/AppError.js";
 import cloudinary from "../../utils/cloudinary.js";
 import bcrypt from "bcrypt";
 export const addDepartment = async (req, res, next) => {
-  try {
     const { name } = req.body;
     const department = await departmentModel.create({ name });
-    return res.json({ message: "success", department });
-  } catch (err) {
-    return res.json({ message: "error", err });
-  }
+    return res.status(201).json({ message: "success", department });
+};
+export const getDepartments = async (req, res, next) => {
+    const deps = await departmentModel.find().select('name -_id');
+    return res.status(200).json({ message: "success", deps });
+};
+export const deleteDepartment = async (req, res, next) => {
+    const { id } = req.params;
+    const dep = await departmentModel.deleteOne({ _id: id });
+    if (!dep.deletedCount) {
+    return res.status(200).json({ message: "department not found" });
+    }
+    return res.status(200).json({ message: "success", dep });
 };
 export const addUser = async (req, res, next) => {
-  try {
     const { name, email, password, phoneNumber, role, depId, officeHours } =
       req.body;
     const { secure_url } = await cloudinary.uploader.upload(req.file.path, {
@@ -42,24 +48,18 @@ export const addUser = async (req, res, next) => {
         img,
       });
     }
-
     return res.status(201).json({ message: "success", user });
-  } catch (err) {
-    return next(new AppError(err.message, 500));
-  }
-};
 
+};
 export const getUsers = async (req, res, next) => {
-  try {
-    const users = await userModel.find({ _id: { $ne: req.userId } });
-    res.status(200).json({ message: "success", users });
-  } catch (err) {
-    next(new AppError(err.message, 500));
-  }
+  const users = await userModel.find({ _id: { $ne: req.userId } }).populate({
+    path: 'department',
+    select: 'name -_id'
+}).select('name email officeHours img phoneNumber depId');
+    return res.status(200).json({ message: "success", users });
 };
 
 export const updateUser = async (req, res, next) => {
-  try {
     const { id } = req.params;
     const { name, email, depId, role, officeHours, phoneNumber, password } =
       req.body;
@@ -71,21 +71,14 @@ export const updateUser = async (req, res, next) => {
     if (!supervisor.modifiedCount) {
       return res.json({ message: "user not found" });
     }
-    res.status(200).json({ message: "success", supervisor });
-  } catch (err) {
-    next(new AppError(err.message, 500));
-  }
+    return res.status(200).json({ message: "success", supervisor });
 };
 export const deleteUser = async (req, res, next) => {
-  try {
     const { id } = req.params;
     const user = await userModel.deleteOne({ _id: id });
     if (!user.deletedCount) {
       res.status(200).json({ message: "user not found" });
     }
     await cloudinary.uploader.destroy(user.img.secure_url);
-    res.status(200).json({ message: "success", user });
-  } catch (err) {
-    next(new AppError(err.message,500));
-  }
+    return res.status(200).json({ message: "success", user });
 };
