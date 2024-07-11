b66fd7569473ed93b42876c54ceaddb28e4f78ae
Tasneem <tasneemgazal5@gmail.com>
2024-07-11 19:48:11 +0300

    remove user folder

--- src/modules/user/user.controller.js
+++
@@ -1,3 +0,0 @@
-export const getUser = (req, res,next) => {
-    return res.json("user");
-}
--- src/modules/user/user.router.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import * as Users from "./user.controller.js"
-const userRouter = Router();
- userRouter.get('/',Users.getUser);
-
-export default userRouter;
