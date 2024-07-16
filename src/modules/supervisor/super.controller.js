6d060ea6e36dcce5893d3338b3e84a3766ff21c7
Tasneem <tasneemgazal5@gmail.com>
2024-07-16 09:51:53 +0300

    create getMySections and assignTask functions

--- src/modules/supervisor/super.controller.js
+++ src/modules/supervisor/super.controller.js
@@ -11,16 +11,30 @@ export const reject = async (req, res, next) => {
   return res.status(201).json({ message: "success", request });
 };
 export const confirm = async (req, res, next) => {
-      const {students} = await requestModel.findById(req.body.requestId)
-      const request = await requestModel.findByIdAndUpdate(
-        req.body.requestId,
-        { state: "accept" },
-        { new: true }
-      );
-      await sectionModel.findByIdAndUpdate(
-        req.body.sectionId, 
-        { students, visible: false },
-        { new: true }
-      );
-      return res.status(201).json({ message: "Success", request});
-  };
+  const { students } = await requestModel.findById(req.body.requestId);
+  const request = await requestModel.findByIdAndUpdate(
+    req.body.requestId,
+    { state: "accept" },
+    { new: true }
+  );
+  await sectionModel.findByIdAndUpdate(
+    req.body.sectionId,
+    { students, visible: false },
+    { new: true }
+  );
+  return res.status(201).json({ message: "Success", request });
+};
+export const getMySections = async (req, res, next) => {
+    const section = await sectionModel.find({ userId: req.userId });
+    return res.status(200).json(section);
+};
+export const assignTask = async (req, res, next) => {
+    const { txt, sections,startDate,endDate } = req.body;
+    const supervisorId = req.userId;
+    if(req.file){
+      const fileTask = await uploadFile(req.file.path);
+      const task = await taskModel.create({ txt, sections,startDate, endDate, file: fileTask,supervisor:supervisorId });
+    }
+    const task = await taskModel.create({ txt, sections,startDate, endDate,supervisor:supervisorId });
+    return res.status(201).json({message:"success",task});
+};
