2432cb04b12a40aaa9016fc2d58b9a2c9ae073b8
Tasneem <tasneemgazal5@gmail.com>
2024-07-16 09:52:20 +0300

    add getSections route

--- src/modules/supervisor/super.router.js
+++ src/modules/supervisor/super.router.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
-import * as Super from"./super.controller.js";
+import * as Super from "./super.controller.js";
 import { auth } from "../../middleware/auth.middleware.js";
 import { role } from "../../utils/role.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 const superRouter = Router();
 
-superRouter.post('/reject',auth([role.supervisor]),asyncHandler(Super.reject))
-superRouter.post('/confirm',auth([role.supervisor]),asyncHandler(Super.confirm))
-
+superRouter.post(
+  "/reject",
+  auth([role.supervisor]),
+  asyncHandler(Super.reject)
+);
+superRouter.post(
+  "/confirm",
+  auth([role.supervisor]),
+  asyncHandler(Super.confirm)
+);
+superRouter.get(
+  "/getSections",
+  auth([role.supervisor]),
+  asyncHandler(Super.getMySections)
+);
 export default superRouter;
