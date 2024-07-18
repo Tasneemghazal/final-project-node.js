455b891a69ff38c26f26c6baa3032e9daacd3a53
Tasneem <tasneemgazal5@gmail.com>
2024-07-18 11:37:24 +0300

    format document and getTask and submit endpoints

--- src/modules/student/student.router.js
+++ src/modules/student/student.router.js
@@ -1,10 +1,31 @@
 import { Router } from "express";
-import * as Student from"./student.controller.js";
+import * as Student from "./student.controller.js";
 import { auth } from "../../middleware/auth.middleware.js";
 import { role } from "../../utils/role.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
+import { checkSection } from "../../middleware/checkSection.middleware.js";
+import uploadFile, { fileTypes } from "../../utils/multer.js";
 const stdRouter = Router();
 
-stdRouter.post('/book',auth([role.student]),asyncHandler(Student.bookSection));
-stdRouter.get('/getSection',auth([role.student]),asyncHandler(Student.getStudentSection));
+stdRouter.post(
+  "/book",
+  auth([role.student]),
+  asyncHandler(Student.bookSection)
+);
+stdRouter.get(
+  "/getSection",
+  auth([role.student]),
+  asyncHandler(Student.getStudentSection)
+);
+stdRouter.get(
+  "/getTask",
+  auth([role.student]),
+  checkSection,
+  asyncHandler(Student.getStudentTask)
+);
+stdRouter.post(
+  "/submit/:taskId/:sectionId",
+  auth([role.student]),uploadFile(fileTypes.pdf).single("file"),
+  asyncHandler(Student.submitTask)
+);
 export default stdRouter;
