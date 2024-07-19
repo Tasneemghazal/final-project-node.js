ca6f8b9cb8a827be82481b79dee639b932dd82f8
Tasneem <tasneemgazal5@gmail.com>
2024-07-19 17:45:04 +0300

    remove isGroup and letestMessage from chatSchema

--- DB/models/chat.model.js
+++ DB/models/chat.model.js
@@ -1,20 +1,12 @@
 import { Schema,Types,model } from "mongoose";
 const chatSchema = new Schema(
   {
-    isGroup: {
-      type: Boolean,
-      default: true,
-    },
     users: [
       {
         type:Types.ObjectId,
         ref: "student",
       },
     ],
-    letestMessage: {
-      type:Types.ObjectId,
-      ref: "message",
-    },
     groupAdmin: {
       type: Types.ObjectId,
       ref: "user",
