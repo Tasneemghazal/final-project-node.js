3565875f0dd2a403b6089f35e463bfdeb248fe60
Tasneem <tasneemgazal5@gmail.com>
2024-07-19 19:33:05 +0300

    add send and get messages routes

--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-
+import * as Chat from "./chat.controller.js";
+import { auth } from "../../middleware/auth.middleware.js";
+import { role } from "../../utils/role.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
 const chatRouter = Router();
 
-
+chatRouter.post("/send/:id",auth([role.student,role.supervisor]), asyncHandler(Chat.sendMessage));
+chatRouter.get("/getMsg/:id",auth([role.student,role.supervisor]), asyncHandler(Chat.getMessages));
 
 export default chatRouter;
